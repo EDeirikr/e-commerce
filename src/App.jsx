0fbc5079e52d2c344cce6dc61d25eba924271926
Erick Eduardo Souza Araujo <erickRexAeternus@gmail.com>
2025-08-23 17:06:55 -0300

    criar contexto

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='font-principal'>
+      <h1>desserts</h1>
+    </div>
   )
 }
 
 export default App
+
+  {/* 
+    <Modal
+      isOpen={isOpen} // estado do modal aberto ou fechado
+      onRequestClose={fecharM}// quando clicar em ESC ou fora do modal
+      overlayClassName={"fundoM"}// estilo do fundo do modal
+      className={"rounded-3xl w-[70vw] h-[70vh]"}// estilos da caixa/conteudo do modal
+      contentLabel=''// texto de acessibilidade do modal
+    > 
+  */}
